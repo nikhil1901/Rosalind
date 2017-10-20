06bc96bc20818455f56892e2241a43092d465e2a
Nikhil Mehta <nikhilmehta1901@gmail.com>
2017-10-20 00:55:58 -0400

    Solution for http://rosalind.info/problems/prot/

---
+++ rosalind_prot.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+rna_codon_table = {
+    "UUU": "F",
+    "CUU": "L",
+    "AUU": "I",
+    "GUU": "V",
+    "UUC": "F",
+    "CUC": "L",
+    "AUC": "I",
+    "GUC": "V",
+    "UUA": "L",
+    "CUA": "L",
+    "AUA": "I",
+    "GUA": "V",
+    "UUG": "L",
+    "CUG": "L",
+    "AUG": "M",
+    "GUG": "V",
+    "UCU": "S",
+    "CCU": "P",
+    "ACU": "T",
+    "GCU": "A",
+    "UCC": "S",
+    "CCC": "P",
+    "ACC": "T",
+    "GCC": "A",
+    "UCA": "S",
+    "CCA": "P",
+    "ACA": "T",
+    "GCA": "A",
+    "UCG": "S",
+    "CCG": "P",
+    "ACG": "T",
+    "GCG": "A",
+    "UAU": "Y",
+    "CAU": "H",
+    "AAU": "N",
+    "GAU": "D",
+    "UAC": "Y",
+    "CAC": "H",
+    "AAC": "N",
+    "GAC": "D",
+    "UAA": "Stop",
+    "CAA": "Q",
+    "AAA": "K",
+    "GAA": "E",
+    "UAG": "Stop",
+    "CAG": "Q",
+    "AAG": "K",
+    "GAG": "E",
+    "UGU": "C",
+    "CGU": "R",
+    "AGU": "S",
+    "GGU": "G",
+    "UGC": "C",
+    "CGC": "R",
+    "AGC": "S",
+    "GGC": "G",
+    "UGA": "Stop",
+    "CGA": "R",
+    "AGA": "R",
+    "GGA": "G",
+    "UGG": "W",
+    "CGG": "R",
+    "AGG": "R",
+    "GGG": "G"
+}
+output = ""
+file = open('rosalind_prot.txt', 'r')
+rna_sequence = file.read()
+# print rna_sequence
+for index in range(0, len(rna_sequence), 3):
+    rna = rna_sequence[index:index+3]
+    rna_convert = rna_codon_table[rna]
+    if(rna_convert == "Stop"):
+        break
+    output = output + rna_convert
+    # print "rna_convert:", rna_convert
+
+print output
