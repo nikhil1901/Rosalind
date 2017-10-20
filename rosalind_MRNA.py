8dcd0d243e7841fcdb5a9637fa1cb89c90b67a03
Nikhil Mehta <nikhilmehta1901@gmail.com>
2017-10-20 00:49:06 -0400

    Solution for http://rosalind.info/problems/mrna/

---
+++ rosalind_MRNA.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+rna_codon_table = {
+    "UUU": "F",
+    "CUU": "L",
+    "AUU": "I",
+    "GUU": "V",
+    "UUC": "F",
+    "CUC": "L",
+    "AUC": "I",
+    "GUC": "V",
+    "UUA": "L",
+    "CUA": "L",
+    "AUA": "I",
+    "GUA": "V",
+    "UUG": "L",
+    "CUG": "L",
+    "AUG": "M",
+    "GUG": "V",
+    "UCU": "S",
+    "CCU": "P",
+    "ACU": "T",
+    "GCU": "A",
+    "UCC": "S",
+    "CCC": "P",
+    "ACC": "T",
+    "GCC": "A",
+    "UCA": "S",
+    "CCA": "P",
+    "ACA": "T",
+    "GCA": "A",
+    "UCG": "S",
+    "CCG": "P",
+    "ACG": "T",
+    "GCG": "A",
+    "UAU": "Y",
+    "CAU": "H",
+    "AAU": "N",
+    "GAU": "D",
+    "UAC": "Y",
+    "CAC": "H",
+    "AAC": "N",
+    "GAC": "D",
+    "UAA": "Stop",
+    "CAA": "Q",
+    "AAA": "K",
+    "GAA": "E",
+    "UAG": "Stop",
+    "CAG": "Q",
+    "AAG": "K",
+    "GAG": "E",
+    "UGU": "C",
+    "CGU": "R",
+    "AGU": "S",
+    "GGU": "G",
+    "UGC": "C",
+    "CGC": "R",
+    "AGC": "S",
+    "GGC": "G",
+    "UGA": "Stop",
+    "CGA": "R",
+    "AGA": "R",
+    "GGA": "G",
+    "UGG": "W",
+    "CGG": "R",
+    "AGG": "R",
+    "GGG": "G"
+}
+output = 1
+file = open('rosalind_mrna.txt', 'r')
+rna_sequence = file.read()
+# print rna_sequence
+for index in range(0, len(rna_sequence)):
+    rna = rna_sequence[index]
+    frequency = 0
+    for dictionaryItem in rna_codon_table.values():
+        if(dictionaryItem == rna):
+            frequency += 1
+    if frequency > 0:
+        output *= frequency
+        output = output % 1000000
+
+frequency = 0
+for dictionaryItem in rna_codon_table.values():
+    if(dictionaryItem == "Stop"):
+        frequency += 1
+if frequency > 0:
+    output *= frequency
+    output = output % 1000000
+print output
