99d57be04efc620a1e00add89420cfb357681bad
Nikhil Mehta <nikhilmehta1901@gmail.com>
2017-10-21 14:02:48 -0400

    Updated rosalind_splc.py with cooments

--- rosalind_splc.py
+++ rosalind_splc.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+"""
+After identifying the exons and introns of an RNA string, we only need to delete the introns and concatenate the exons to form a new string ready for translation.
+Given: A DNA string ss (of length at most 1 kbp) and a collection of substrings of ss acting as introns. All strings are given in FASTA format.
+Return: A protein string resulting from transcribing and translating the exons of ss. (Note: Only one solution will exist for the dataset provided.)
+
+Sample Dataset
+
+>Rosalind_10
+ATGGTCTACATAGCTGACAAACAGCACGTAGCAATCGGTCGAATCTCGAGAGGCATATGGTCACATGATCGGTCGAGCGTGTTTCAAAGTTTGCGCCTAG
+>Rosalind_12
+ATCGGTCGAA
+>Rosalind_15
+ATCGGTCGAGCGTGT
+
+
+Sample Output
+
+MVYIADKQHVASREAYGHMFKVCA
+"""
+
 rna_codon_table = {
     "UUU": "F",
     "CUU": "L",
@@ -67,7 +87,7 @@ rna_codon_table = {
 }
 output = ""
 
-
+# open input file
 fileName = 'rosalind_splc.txt'
 with open(fileName) as file:
     content = file.readlines()
@@ -78,6 +98,8 @@ graphElements = list()
 nameFlag = False
 stringSet = set()
 sequenceCount = 0
+
+# take input lines from the file given in FASTA format
 for line in content:
     if(line.startswith('>')):
         temp = line[:]
@@ -91,15 +113,7 @@ for line in content:
             temp = line[:]
             temp = temp.translate(None, '\n')
             prevSequence = str(sequences[len(sequences) - 1][:])
-            # print temp
-            # print prevSequence
-            # print len(sequences) - 1
-            # print ""
             prevSequence = prevSequence + temp
-            # print prevSequence
-            # print prevSequence
-            # print len(sequences) - 1
-            # print ""
             sequences[len(sequences) - 1] = prevSequence
         else:
             temp = line[:]
@@ -108,31 +122,24 @@ for line in content:
             sequenceCount += 1
             nameFlag = False
 
-# print len(stringNames)
-# print sequences[0]
-# print len(stringSet)
 rna_sequence = ""
 rna_sequence = sequences[0][:]
 del sequences[0]
 
-
+# replace T with U, since in RNA, Thymine gets replaced by Uracil
 rna_sequence = rna_sequence.replace("T", "U")
-# print rna_sequence
 
 for sequence in sequences:
     sequence = sequence.replace("T", "U")
     rna_sequence = rna_sequence.replace(sequence, "")
 
-# print rna_sequence
-
-
-# print rna_sequence
+# convert until we get Stop sequence
 for index in range(0, len(rna_sequence), 3):
     rna = rna_sequence[index:index+3]
     rna_convert = rna_codon_table[rna]
     if(rna_convert == "Stop"):
         break
     output = output + rna_convert
-    # print "rna_convert:", rna_convert
 
+# print the output
 print output
